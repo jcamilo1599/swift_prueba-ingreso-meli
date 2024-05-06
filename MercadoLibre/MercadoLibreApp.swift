6d1e1bdafbdaa075983cf32ec7a35127733d0812
Juan Camilo <99774186+jcamilo1599@users.noreply.github.com>
2024-05-06 01:21:31 -0500

    Initial Commit

---
+++ MercadoLibre/MercadoLibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoLibreApp.swift
+//  MercadoLibre
+//
+//  Created by Juan Camilo Marín Ochoa on 6/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MercadoLibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
